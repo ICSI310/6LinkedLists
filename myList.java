94259cac5ace3a84f6eeeec480d0c588734a4a96
cmagnus <cmagnus@gmail.com>
2016-03-10 13:55:53 -0500

    added print statements to Mylist for debugging

--- myList.java
+++ myList.java
@@ -6,18 +6,21 @@
   
   // Constructors
   public MyList() {
+    System.out.println("MyList: Default constructor");
     this.head = new Node();
     this.current = head;
   }
   
   // Methods
   public void add(Node newNode) {
+    System.out.println("MyList: add");
     // just add to head of list for now
     newNode.setNext(this.head.getNext());
     this.head = newNode;  
   }
   
   public void print() {
+    System.out.println("MyList: print");
     while(null != this.current.getNext()) {
       this.current.print();
       this.current = this.current.getNext();
