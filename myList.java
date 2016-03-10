048309b38a66ef50957a3addcf8a892a77ff2932
cmagnus <cmagnus@gmail.com>
2016-03-10 14:35:04 -0500

    Where we left off. Have a good spring break!

--- myList.java
+++ myList.java
@@ -16,13 +16,31 @@
     System.out.println("MyList: add");
     this.head.debug();
     newNode.debug();
-    //System.out.println("newNode: " + newNode + " Data: " + newNode.getData());
-    //System.out.println("head: " + this.head + " Data: " + newNode.getData());
-    // just add to head of list for now
+     // just add to head of list for now
     newNode.setNext(this.head);
     newNode.debug();
     this.head = newNode; 
     this.head.debug();
+  }
+  
+  public void insert(int index, Node newNode) {
+    if (index == 0) this.add(newNode);
+    else{
+      // traverse to new node
+      // set newnode's next
+      newNode.setNext(????);
+      // set node before newnode's next to newnode
+    } 
+  }
+  
+  public Node get(int index) {
+    this.current = this.head;
+    // how do we fix this so it stops at index?
+    // add a counter?
+    while(null != this.current){
+      this.current = this.current.getNext();
+    }
+    return(this.current);
   }
   
   // traverses and prints
@@ -30,7 +48,7 @@
     System.out.println("MyList: print");
     this.current = this.head;
     this.current.debug();
-    while(null != this.current.getNext()) {
+    while(null != this.current){
       this.current.debug();
       this.current.print();
       this.current = this.current.getNext();
